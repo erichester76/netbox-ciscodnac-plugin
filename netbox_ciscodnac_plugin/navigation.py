eb7f44326f336258da37a64ab6b5e8f5b99eeac6
Eric Hester <eric.hester@umbrella.tech>
2024-10-22 12:33:35 -0400

    .

--- netbox_ciscodnac_plugin/navigation.py
+++ netbox_ciscodnac_plugin/navigation.py
@@ -5,19 +5,16 @@ menu_items = (
     PluginMenuItem(
         link="plugins:netbox_ciscodnac_plugin:status",
         link_text="Status",
-        permissions=["netbox_ciscodnac_plugin.admin_full"],
         buttons=(
             PluginMenuButton(
                 link="plugins:netbox_ciscodnac_plugin:sync_full",
                 title="Settings",
                 icon_class="mdi mdi-all-inclusive",
-                permissions=["netbox_ciscodnac_plugin.admin_full"],
             ),
             PluginMenuButton(
                 link="plugins:netbox_ciscodnac_plugin:settings",
                 title="Settings",
                 icon_class="mdi mdi-cog",
-                permissions=["netbox_ciscodnac_plugin.admin_full"],
             ),
         ),
     ),
